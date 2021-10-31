087c1e0e4f18fbf89490d3452edbfd56fc2a13f5
James Lefrère <james.lefrere@mstable.org>
2021-10-31 14:36:46 +0100

    WIP: Ulam

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ChainId, DAppProvider, Config } from '@usedapp/core'
+import { createGlobalStyle } from 'styled-components'
 
 import { App } from './App'
+import { Ulam } from './Ulam'
 
 const config: Config = {
   readOnlyChainId: ChainId.Mainnet,
@@ -14,11 +16,38 @@ const config: Config = {
   },
 }
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background: black;
+    color: white;
+  }
+  
+  #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 2rem;
+    max-width: 90ch;
+    align-items: center;
+  }
+`
+
+// <DAppProvider config={config}>
+//   <App />
+// </DAppProvider>
 ReactDOM.render(
   <React.StrictMode>
-    <DAppProvider config={config}>
-      <App />
-    </DAppProvider>
+    <GlobalStyle />
+    <main>
+      <Ulam />
+    </main>
   </React.StrictMode>,
   document.getElementById('root'),
 )
